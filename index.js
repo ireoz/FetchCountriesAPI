11a71a26da2b2f9fa76114d9477b9a0ec519d83d
Andrew Higgins <andrew.higgins2@hotmail.com>
2022-09-26 21:14:05 +0100

    While using Promises, Fetch Country name and flag from Countries API and displayed data on the browser

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 
 function fetchCountries(){
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://restcountries.com/v3.1/all', true);
-    xhr.send();
-    xhr.onload = function() {
-        if(xhr.status = 200){
-            let countries = JSON.parse(xhr.response);
+
+     const promise = new Promise((resolve,reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://restcountries.com/v3.1/all', true);
+        xhr.send();
+        xhr.onload = function() {
+            if(xhr.status == 200){
+                resolve(JSON.parse(xhr.response))
+            }
+            else reject("something went wrong fetching data"); 
+            }
+        })
+
+        promise.then(fileContent => {
             let element = document.querySelector('.container');
-            let img
-            countries.forEach(country => {
-                let countryCard = document.createElement('div');
-                let imageCard = document.createElement('img');
-                countryCard.innerHTML = country.name.official;
-                imageCard.setAttribute('src', country.flags.png);
-                // console.log(country.flags.png)
-                element.appendChild(countryCard);
-                element.appendChild(imageCard);
-
-                
-
+                    fileContent.forEach(country => {
+                    let countryCard = document.createElement('div')
+                    let imageCard = document.createElement('img');
+                     countryCard.innerHTML = country.name.official;
+                     imageCard.setAttribute('src', country.flags.png);
+                     element.appendChild(countryCard);
+                     element.appendChild(imageCard);
             })
-        }
+        }).catch(value => alert(value));
+
     }
-    
-   
-}
+
+
